4bc590f30fcd077d90f8e73bd5af41a367b79695
stiven_cruz <bseguracruz@yahoo.com>
2023-05-04 12:45:10 -0500

    normalizacion

---
+++ utils.js
@@ -0,0 +1,8 @@
+import { dirname } from 'path'
+import { fileURLToPath } from 'url'
+
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = __filename
+
+export {__filename , __dirname }

d1e00625a68f10019f9cfb90ce57ab520c547eba
stiven_cruz <bseguracruz@yahoo.com>
2023-05-07 19:21:07 -0500

    modelo y data authors

---
+++ models/Author.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    last_name: { type: String, required: false },
+    city: { type: String, required: true },
+    country: { type: String, required: true },
+    date: { type: Date },
+    photo: { type: String, required: true },
+    user_id: { type: mongoose.Types.ObjectId, ref: "users", required: true },
+    active: { type: Boolean, required: true },
+  },
+  { timestamps: true }
+);
+
+export const Author = mongoose.model(schema,'authors')
+
---
+++ models/data/authors.js
@@ -0,0 +1,45 @@
+export let author1 = {
+    name: "alejandro",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true
+}
+export let author2 = {
+    name: "lucas",
+    last_name: "silva",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true
+}
+export let author3 = {
+    name: "jose",
+    last_name: "lopez",
+    city: "villa carlos paz",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true
+}
+export let author4 = {
+    name: "eric",
+    city: "resistencia",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true
+}
+export let author5 = {
+    name: "igna",
+    last_name: "borraz",
+    city: "rosario",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true
+}
+export let author6 = {
+    name: "silvina",
+    city: "buenos aires",
+    country: "argentina",
+    photo: "https://conceptodefinicion.de/wp-content/uploads/2016/01/Perfil2.jpg",
+    active: true
+}

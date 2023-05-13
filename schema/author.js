f1978d7acbd784e1ad4e494a2c7899535ef53a8d
stiven_cruz <bseguracruz@yahoo.com>
2023-05-13 11:56:58 -0500

    validador

--- schema/author.js
+++ schema/author.js
@@ -1,1 +1,30 @@
- import Joi from "joi";
+import Joi from "joi-oid";
+
+let authorSchema = Joi.object({
+  name: Joi.string().required().min(2).max(30).message({
+    "string.min": "the name must be at least 2 characteres",
+    "string.max": "the name must not have more than 30 characters",
+    "string.required": "the lastname is required",
+  }),
+  last_name: Joi.string().required().min(2).max(30).message({
+    "string.min": "the lastname must be at least 2 characteres",
+    "string.max": "the lastname must not have more than 30 characters",
+    "string.required": "the lastname is required",
+  }),
+  city: Joi.string().required().min(2).max(30).message({
+    "string.min": "the city must be at least 2 characteres",
+    "string.max": "the city must not have more than 30 characters",
+    "string.required": "the city is required",
+  }),
+  country: Joi.string().required().min(2).max(30).message({
+    "string.min": "the country must be at least 2 characteres",
+    "string.max": "the country must not have more than 30 characters",
+    "string.required": "the country is required",
+  }),
+  date: Joi.date(),
+  photo: Joi.string().required().min(1).uri().message({
+    "string.min": "the image must be at least 1 characteres",
+  }),
+  user_id: Joi.objectId().required(),
+});
+export default authorSchema;

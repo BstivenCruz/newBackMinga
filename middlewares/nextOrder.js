aa6e5a47ee78c6fff6b5365a652842d7f55fc360
stiven_cruz <bseguracruz@yahoo.com>
2023-05-19 16:21:40 -0500

    order exist is ready

--- middlewares/nextOrder.js
+++ middlewares/nextOrder.js
@@ -3,7 +3,6 @@ async function nextOrder(req, res, next) {
   if (req.body.order) {
     return next();
   }
-
   const chapter = await Chapter.find().sort({ order: -1 }).limit(1);
   req.body.order = chapter.order + 1;
   return next();

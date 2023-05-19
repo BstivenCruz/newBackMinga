c7464edf3014ee949d1309ca08ad1dd3e71e15d7
stiven_cruz <bseguracruz@yahoo.com>
2023-05-19 10:26:21 -0500

    find id ready

--- middlewares/findsId.js
+++ middlewares/findsId.js
@@ -4,23 +4,20 @@ import defaultResponse from "../config/response.js";
 
 async function findsId(req, res, next) {
   const author = await Author.findOne({ user_id: req.user_id });
-  if (!author) {
-    req.body.success = false;
-    req.body.sc = 400;
-    req.body.data = "author does not exist!";
-    return defaultResponse(req, res);
-  }
-  req.body.author_id = author._id;
-
   const company = await Company.findOne({ user_id: req.user_id });
-  if (!company) {
+
+  if (author) {
+    req.body.author_id = author._id;
+    return next();
+  } else if (company) {
+    req.body.company_id = company._id;
+    return next();
+  } else {
     req.body.success = false;
     req.body.sc = 400;
-    req.body.data = "company does not exist!";
+    req.body.data = "not exist!";
     return defaultResponse(req, res);
   }
-  req.body.company_id = company._id;
-  next();
 }
 
-export default findsId
+export default findsId;

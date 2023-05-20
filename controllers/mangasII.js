dce4c0ba84a6407aff61f46a47abd29ab7b1c312
stiven_cruz <bseguracruz@yahoo.com>
2023-05-20 14:27:21 -0500

    get for author

---
+++ controllers/mangasII.js
@@ -0,0 +1,34 @@
+import { Manga } from "../models/Manga.js";
+import defaultResponse from "../config/response.js";
+const get = {
+  getForAuthor: async (req, res, next) => {
+    try {
+      const { idAuthor } = req.params;
+      const query = req.query.new !== "false";
+      const sort = query ? { createdAt: -1 } : { createdAt: 1 };
+      const contador = await Manga.countDocuments({ author_id: idAuthor });
+      const limit = contador < 4 ? contador : Math.ceil(contador / 2);
+      if (contador === 0) {
+        req.body.success = false;
+        req.body.sc = 400;
+        req.body.data = "there are not mangas created by this author";
+        return defaultResponse(req, res);
+      }
+      let manga = await Manga.find(
+        { author_id: idAuthor },
+        "-_id -author_id -createdAt -updatedAt -__v"
+      )
+        .sort(sort)
+        .limit(limit)
+        .skip(0);
+      req.body.success = true;
+      req.body.sc = 201;
+      req.body.data = manga;
+      return defaultResponse(req, res);
+    } catch (error) {
+      next(error);
+    }
+  },
+};
+
+export default get;
--- routes/manga.js
+++ routes/manga.js
@@ -3,13 +3,15 @@ import category from "./category.js";
 import chapter from "./chapter.js";
 import mangas from "../controllers/mangas.js";
 const {read} = mangas
-//var express = require('express');
+import get from "../controllers/mangasII.js";
+const { getForAuthor } = get
+
 const router = express.Router();
 
 /* GET users listing. */
 router.get("/", read);
 router.use("/categories", category);
 router.use("/chapters", chapter);
-router.get('/authors/:idAuthor')
+router.get('/authors/:idAuthor',getForAuthor)
 
 export default router;

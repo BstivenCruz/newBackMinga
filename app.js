88d282c2531c239a8be813b53f01e39d9e03f3bd
stiven_cruz <bseguracruz@yahoo.com>
2023-05-21 14:50:15 -0500

    fix error

--- app.js
+++ app.js
@@ -1,44 +1,28 @@
-import 'dotenv/config.js'
-import  './config/database.js';
-import createError from 'http-errors';
-import express from 'express';
-import path from 'path';
-import cookieParser from 'cookie-parser';
-import logger from 'morgan';
+import "dotenv/config.js";
+import "./config/database.js";
+import createError from "http-errors";
+import express from "express";
+import path from "path";
+import cookieParser from "cookie-parser";
+import logger from "morgan";
 
-import indexRouter from './routes/index.js';
-import usersRouter from './routes/user.js';
-import { __dirname } from './utils.js';
+import indexRouter from "./routes/index.js";
+import { __dirname } from "./utils.js";
 
 const app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
+app.use(notFoundHandler)
+app.use(errorHandler)
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
-});
+app.use("/", indexRouter);
 
 export default app;

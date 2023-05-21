fae44b447f2478c8e40ac8bc5f1785233d5e7497
stiven_cruz <bseguracruz@yahoo.com>
2023-05-21 14:51:23 -0500

    fix error

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 import "dotenv/config.js";
 import "./config/database.js";
-import createError from "http-errors";
 import express from "express";
 import path from "path";
 import cookieParser from "cookie-parser";
